2266c1c67eff90858fb178951fe2b3d73fcc46d6
ScottTZhang <tingzhang2013@gmail.com>
2024-10-15 03:26:58 -0700

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connect local db success');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
