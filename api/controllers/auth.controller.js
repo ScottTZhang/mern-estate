1215a49a505df2db6f1fa65a263b7501791d173d
ScottTZhang <tingzhang2013@gmail.com>
2024-10-21 01:22:29 -0700

    Create sign in API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   //get information from browser
@@ -11,11 +12,37 @@ export const signup = async (req, res, next) => {
   const newUser = new User({ username, email, password: hashedPassword });
 
   try {
-    await newUser.save();
+    await newUser.save();  //mongodb function
     res.status(201).json("User created successfully.");
   } catch (error) {
     //res.status(500).json(error.message);
     next(error);
     //next(errorHandler(550, 'Error from the function'));
+  }
+};
+
+export const signin = async(req, res, next) => {
+  const {email, password} = req.body;
+  try {
+    const validUser = await User.findOne({email}); //mongodb function
+    if(!validUser) {
+      return next(errorHandler(404, 'User not found'));
+    }
+
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, 'Wrong password'));
+    }
+    
+    //need to create cookie, JSON Web Token (JWT)
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+    const {password: pass, ...rest} = validUser._doc; //deconstructure
+    res
+      .cookie('acces_token', token, {httpOnly: true, expires: new Date(Date.now() + 24 + 60 * 60 * 30)})
+      .status(200)
+      .json(rest); //need to remove password
+
+  } catch (error) {
+    next(error); //will use next() in index.js middleware app.use((err, req, res, next) =>{})
   }
 };
