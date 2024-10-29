bbe73a8eae4cf674b10863d25bb9ad2d7ea51e3d
ScottTZhang <tingzhang2013@gmail.com>
2024-10-29 03:21:39 -0700

    Add Redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { store } from './redux/store.js'
+import { persistor, store } from './redux/store.js'
 import { Provider } from 'react-redux'; // makes the Redux store available to any nested components that need to access the Redux store.
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
     <App />
+  </PersistGate>
   </Provider>,
 )
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice.js';
+import {persistReducer, persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({user: userReducer});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {user: userReducer},
+  reducer: {user: persistedReducer},
   middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware({serializableCheck: false}),
 });
@@ -11,3 +22,5 @@ console.log(store.getState());
 //reducer: If this is a single function, it will be directly used as the root reducer for the store. If it is an object of slice reducers, like {users : usersReducer, posts : postsReducer}, configureStore will automatically create the root reducer by passing this object to the Redux combineReducers utility.
 //middleware: A callback which will receive getDefaultMiddleware as its argument, and should return a middleware array.
 //serializableCheck: to avoid getting an error for not serializing variables. A custom middleware that detects if any non-serializable values have been included in state or dispatched actions, modeled after redux-immutable-state-invariant. Any detected non-serializable values will be logged to the console.
+
+export const persistor = persistStore(store);
