2b682e595ad9a37913fc146b650295b6aeb2997e
ScottTZhang <tingzhang2013@gmail.com>
2024-12-16 02:05:09 -0800

    Complete create listing page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/header';
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
 
 function App() {
   return (
@@ -17,7 +18,8 @@ function App() {
         <Route path='/sign-up' element={<SignUp />}></Route>
         <Route path='/about' element={<About />}></Route>
         <Route element={<PrivateRoute />}>
-          <Route path='/profile' element={<Profile />}></Route>
+        <Route path='/profile' element={<Profile />}></Route>
+        <Route path='/create-listing' element={<CreateListing />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
