cac56c633eb5bb0420b418a3613e9cc4d77022d5
ScottTZhang <tingzhang2013@gmail.com>
2025-02-10 23:20:50 -0800

    Complete search page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,12 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
-import Header from './components/header';
+import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
+import Search from './pages/Search';
 
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
           <Route path='/update-listing/:listingId' element={<UpdateListing />}></Route>
         </Route>
         <Route path='/listing/:listingId' element={<Listing />}></Route>
+        <Route path='/search' element={<Search />}></Route>
       </Routes>
     </BrowserRouter>
   );
